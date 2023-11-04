6d1e16c46f45e9190823325c8c1a15f615e0310a
Cole Hersh <cole900@att.net>
2023-11-04 12:04:02 -0700

    Added taking off and returing

--- Pymavtest.py
+++ Pymavtest.py
@@ -9,7 +9,20 @@ the_connection.mav.command_long_send(the_connection.target_system, the_connectio
 # print(msg
 
 #Takes Off
-the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 37.41337964719044, -121.9966774838903, 100)
+#Lat test  37.41337964719044
+#Long Test -121.9966774838903
+the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 37.41337964719044, -121.9966774838903, 50)
 msg = the_connection.recv_match(type='COMMAND_ACK', blocking=True)
 print(msg)
+run = True
+while run:
+    msg = the_connection.recv_match(
+        type='LOCAL_POSITION_NED', blocking=True)
+    print
+    temp = str(msg).split()
+    temp = temp[12]
+    temp = float(temp[:len(temp) - 1])
+    #print(temp)
+    if(temp <= -49):
+        run = False
 the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component, mavutil.mavlink.MAV_CMD_NAV_RETURN_TO_LAUNCH, 0, 0, 0, 0, 0, 0, 0, 0)
