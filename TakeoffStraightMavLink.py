ee38c3349b9ad6b3616f09da1f1c2bf26d04efd3
Cole Hersh <cole900@att.net>
2023-11-11 11:27:49 -0700

    fixed spelling error in comment

--- TakeoffStraightMavLink.py
+++ TakeoffStraightMavLink.py
@@ -28,7 +28,7 @@ lon /= 10 ** 7
 the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, lat, lon, 50)
 msg = the_connection.recv_match(type='COMMAND_ACK', blocking=True)
 #print(msg)
-# This runs until the desired Altitude is reaches
+# This runs until the desired Altitude is reached
 run = True
 while run:
     msg = the_connection.recv_match(
