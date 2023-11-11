a95e53a0d900ce2000925242909b918d2b0fa01e
Cole Hersh <cole900@att.net>
2023-11-11 11:26:02 -0700

    added straight takeoff

--- TakeoffStraightMavLink.py
+++ TakeoffStraightMavLink.py
@@ -11,18 +11,30 @@ the_connection.mav.command_long_send(the_connection.target_system, the_connectio
 #Takes Off
 #Lat test  37.41337964719044
 #Long Test -121.9966774838903
-the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 37.41337964719044, -121.9966774838903, 50)
+
+msg = the_connection.recv_match(
+        type='GLOBAL_POSITION_INT', blocking=True)
+msg = str(msg).split()
+print(msg)
+lat = msg[6]
+lat = float(lat[:len(lat) - 1])
+lon = msg[9]
+lon = float(lon[:len(lon) - 1])
+lat /= 10 ** 7
+lon /= 10 ** 7
+
+the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, lat, lon, 50)
 msg = the_connection.recv_match(type='COMMAND_ACK', blocking=True)
 print(msg)
 run = True
 while run:
     msg = the_connection.recv_match(
         type='LOCAL_POSITION_NED', blocking=True)
-    print
+    #print(msg)
     temp = str(msg).split()
     temp = temp[12]
     temp = float(temp[:len(temp) - 1])
-    print(temp)
+    #print(temp)
     # altitude is off by less than 2 meters
     if(temp <= -48):
         run = False
