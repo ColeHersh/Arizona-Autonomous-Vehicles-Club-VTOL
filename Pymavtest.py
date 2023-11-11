d0ee9e25960579bdc17ee3a87248f12009696499
Cole Hersh <cole900@att.net>
2023-11-11 10:53:32 -0700

    added takeoff and RTL?

--- Pymavtest.py
+++ Pymavtest.py
@@ -22,7 +22,7 @@ while run:
     temp = str(msg).split()
     temp = temp[12]
     temp = float(temp[:len(temp) - 1])
-    #print(temp)
+    print(temp)
     if(temp <= -49):
         run = False
 the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component, mavutil.mavlink.MAV_CMD_NAV_RETURN_TO_LAUNCH, 0, 0, 0, 0, 0, 0, 0, 0)
