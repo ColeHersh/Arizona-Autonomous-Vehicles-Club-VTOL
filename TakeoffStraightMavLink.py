6567a547b4c3b6a6c90702f7174230f5f30b4a73
Cole Hersh <cole900@att.net>
2023-11-11 11:27:15 -0700

    addes some comments

--- TakeoffStraightMavLink.py
+++ TakeoffStraightMavLink.py
@@ -12,6 +12,8 @@ the_connection.mav.command_long_send(the_connection.target_system, the_connectio
 #Lat test  37.41337964719044
 #Long Test -121.9966774838903
 
+
+# This gets the current lat and lon
 msg = the_connection.recv_match(
         type='GLOBAL_POSITION_INT', blocking=True)
 msg = str(msg).split()
@@ -25,7 +27,8 @@ lon /= 10 ** 7
 
 the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, lat, lon, 50)
 msg = the_connection.recv_match(type='COMMAND_ACK', blocking=True)
-print(msg)
+#print(msg)
+# This runs until the desired Altitude is reaches
 run = True
 while run:
     msg = the_connection.recv_match(
