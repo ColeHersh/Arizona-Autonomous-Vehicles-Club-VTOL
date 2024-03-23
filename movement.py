c1f22dc5cfbb77093b117d645f1d418f8e752c36
Cole Hersh <cole900@att.net>
2024-03-23 10:19:42 -0700

    j

--- movement.py
+++ movement.py
@@ -12,7 +12,7 @@ plane.get_global_info()
 mission_items = []
 mission_items.append(mission_item(0, 0, plane.get_lat() + 0.0001, plane.get_lon() + 0.0001, 50))
 mission_items.append(mission_item(1, 0, plane.get_lat() + 0.0003, plane.get_lon() + 0.0001, 50))
-mission_items.append(mission_item(2, 0, plane.get_lat() + 0.0001, plane.get_lon() - .0001, 50))
+mission_items.append(mission_item(2, 0, plane.get_lat() + 0.0001, plane.get_lon() - .0001, 50, mavutil.mavlink.MAV_CMD_NAV_LOITER_UNLIM))
 mission_items.append(mission_item(3, 0, plane.get_lat(), plane.get_lon(), 50, mavutil.mavlink.MAV_CMD_NAV_LAND))
 
 plane.upload_mission(mission_items)
@@ -20,9 +20,14 @@ plane.upload_mission(mission_items)
 plane.start_mission()
 
 # Upload interrup mission item, set the sequence number
-#while(1):
-    #print(plane.get_curr_item_squence(), "erfr3f")
-
+inter = True
+while(1):
+    print(plane.get_curr_item_squence())
+    if(plane.get_curr_item_squence() == 2 and inter):
+        plane.upload_mission([mission_item(1, 0, plane.get_lat(), plane.get_lon() + 0.0001, 50, mavutil.mavlink.MAV_CMD_NAV_LAND)])
+        plane.start_mission()
+        #plane.set_curr_waypoint(0)
+        inter = False
 # print("Attempting go to")
 # plane.get_global_info()
 # plane.goto(plane.get_lat() + 0.0001 , plane.get_lon()  + 0.0001,50)
