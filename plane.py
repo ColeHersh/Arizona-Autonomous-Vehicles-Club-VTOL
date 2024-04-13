6d901e5833e1f842ade8dd74ff32eda361988424
Cole Hersh <cole900@att.net>
2024-04-13 14:23:10 -0700

    added doc strings

--- plane.py
+++ plane.py
@@ -4,7 +4,16 @@ from geofence_competition import set_geofence_comp
 import asyncio
 
 class Plane:
+    """
+    This class holds the commands and variables
+    for the aircraft
+    """
+    
     def __init__(self, connection):
+        """
+        This intializes the plan
+        :param connection - the connection to the aircraft
+        """
         self._the_connection = connection
         self._global_info = []
         self._altitude = 0
@@ -14,13 +23,25 @@ class Plane:
         self.landed = False
     
     def get_heartbeat(self):
+        """
+        This method prints the connection and its info.
+        Used to confirm connection to the aircraft
+        """
         self._the_connection.wait_heartbeat()
         print("Heartbeat from system (system %u component %u)" % (self._the_connection.target_system, self._the_connection.target_component))
         
     def arm(self):
+        """
+        Arms the aricraft so it is ready to recive commands
+        """
         self._the_connection.mav.command_long_send(self._the_connection.target_system, self._the_connection.target_component, mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, 1, 0, 0, 0, 0, 0, 0)
     
     def upload_mission(self, mission_items):
+        """
+        Takes in mission and uploads it to the plane
+        
+        :param mission_items - an array of mission_item objects
+        """
         n = len(mission_items)
         print("Sending mission")
         self._the_connection.mav.mission_count_send(self._the_connection.target_system, self._the_connection.target_component, n, 0)
@@ -45,44 +66,22 @@ class Plane:
                 self.acknowledge('MISSION_REQUEST')
         self.acknowledge('MISSION_ACK')
     
-
-
-
-
-    def goto(self,lat,lon,alt, FLIGHT_VELOCITY_X = 0, FLIGHT_VELOCITY_Y = 0, FLIGHT_VELOCITY_Z = 0):
-        # self._the_connection.mav.command_int_send(self._the_connection.target_system, 
-        #                                           self._the_connection.target_component, 
-        #                                           mavutil.mavlink.MAV_FRAME_GLOBAL_INT, 
-        #                                           mavutil.mavlink.MAV_CMD_DO_SET_HOME, 
-        #                                           0, 0, 
-        #                                           0, 0, 
-        #                                           0, 0, 
-        #                                           int(lat*1e7), int(lon*1e7), alt)
-        
-        # self._the_connection.mav.command_int_send(self._the_connection.target_system, 
-        #                                           self._the_connection.target_component, 
-        #                                           mavutil.mavlink.MAV_FRAME_GLOBAL, 
-        #                                           mavutil.mavlink.MAV_CMD_DO_REPOSITION, 
-        #                                           0,
-        #                                           0, float(-1), 
-        #                                           mavutil.mavlink.MAV_DO_REPOSITION_FLAGS_CHANGE_MODE, 
-        #                                           0.0, float("nan"), 
-        #                                           int(lat*1e7), int(lon*1e7), alt)
-        
-        # could use MAV_CMD_DO_REPOSITION for this
-        self._the_connection.mav.send(mavutil.mavlink.MAVLink_set_position_target_global_int_message(
-            10, self._the_connection.target_system, self._the_connection.target_component, 
-            mavutil.mavlink.MAV_FRAME_GLOBAL_INT, 
-            int(0b111000000000), int(lat*10**7), int(lon*10**7), alt, FLIGHT_VELOCITY_X, FLIGHT_VELOCITY_Y, FLIGHT_VELOCITY_Z, 
-            0, 0, 0, 0, 0))
-        msg = self._the_connection.recv_match(type='COMMAND_ACK', blocking=True)
-        print(msg)
         
     def acknowledge(self, command = 'COMMAND_ACK'):
+        """
+        Prints the command acknownledgement and result
+        
+        :param command - the command to be acknowledged
+        """
         msg = self._the_connection.recv_match(type=command, blocking=True)
         print(msg)
 
     def get_curr_item_squence(self):
+        """
+        Gets the current waypoint's sequence number
+        
+        :return an integer representing the current waypont's postion in the sequence
+        """
         return self._the_connection.waypoint_current()
         
     def get_state(self):
@@ -94,13 +93,12 @@ class Plane:
         return msg
         
     def abort(self):
-        # MAV_CMD_DO_PAUSE_CONTINUE
-        # MAV_CMD_DO_REPOSITION
+        """
+        Pauses the current mission for interrupt.
+        Allows for CV to operate and direct aircraft
+        """
         self.get_global_info()
-       # self._the_connection.mav.command_long_send(self._the_connection.target_system, self._the_connection.target_component,  mavutil.mavlink.MAV_CMD_DO_PAUSE_CONTINUE, 0, 
-        #                                           0, 0, 0, 0, 0, 0, 0)
-        self._the_connection.mav.command_long_send(self._the_connection.target_system, self._the_connection.target_component,  mavutil.mavlink.MAV_CMD_DO_REPOSITION, 0, 
-                                                  -1, 0x00000001, 0, float("NaN"),  float("NaN"), float("NaN"), 50)
+        self.move()
         msg = self._the_connection.recv_match(type='COMMAND_ACK', blocking=True)
         print(msg)
     
@@ -112,11 +110,18 @@ class Plane:
         print(msg)
   
     def move(self):
-      self._the_connection.mav.command_long_send(self._the_connection.target_system, self._the_connection.target_component,  mavutil.mavlink.MAV_CMD_DO_REPOSITION, 0, 
+        """
+        Moves the aircraft to the desired position
+        """
+             
+        self._the_connection.mav.command_long_send(self._the_connection.target_system, self._the_connection.target_component,  mavutil.mavlink.MAV_CMD_DO_REPOSITION, 0, 
                                                   -1, 0x00000001, 0, float("NaN"),  self.get_lat() + 0.0004, float("NaN"), 10)
     
     
     def takeoff(self):
+        """
+        Takes off at the current location
+        """
         lat = self.get_lat()
         lon = self.get_lon()
         self._the_connection.mav.command_long_send(self._the_connection.target_system, self._the_connection.target_component, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, lat, lon, 50)
@@ -131,42 +136,45 @@ class Plane:
                 run = False
                 
     def rtl(self):
+        """
+        Lands at the takeoff location
+        """
         self._the_connection.mav.command_long_send(self._the_connection.target_system, self._the_connection.target_component, mavutil.mavlink.MAV_CMD_NAV_RETURN_TO_LAUNCH, 0, 0, 0, 0, 0, 0, 0, 0)
         self.landed = True
+        
     def set_curr_waypoint(self, seq_num):
+        """
+        Sets the current waypoint to move to in the mission
+        
+        :param the desired waypoint's sequence number
+        """
         self._the_connection.waypoint_set_current_send(seq_num)
         
     def start_mission(self):
+        """
+        Starts the mission.  Requirement- a mission must be uploaded
+        """
         print("Starting mission")
         self._the_connection.mav.command_long_send(self._the_connection.target_system,
                                                    self._the_connection.target_component,
                                                    mavutil.mavlink.MAV_CMD_MISSION_START,
                                                    0, 0, 0, 0, 0, 0, 0, 0)
-        
-        #self._the_connection.mav.command_long_send(self._the_connection.target_system,
-                                                   #self._the_connection.target_component, mavutil.mavlink.MAV_CMD_DO_SET_MISSION_CURRENT,  3, 0, 0, 0, 0, 0, 0, 0)
-        #self._the_connection.waypoint_set_current_send(2)
-        # gets curre waypoint - tries to keep it at first one
-       # while (1):
-           # way = self._the_connection.waypoint_current()
-           # print(way)
-            #if(way == 2):
-                 #self._the_connection.waypoint_set_current_send(0)
-           # if way != 2:
-              #  self._the_connection.mav.command_long_send(self._the_connection.target_system,
-                #                                   self._the_connection.target_component, mavutil.mavlink.MAV_CMD_DO_SET_MISSION_CURRENT,  0, 0, 0, 0, 0, 0, 0, 0)
-                #self._the_connection.waypoint_set_current_send(1)
 
-    '''
-    Lands plane at the current GPS Cords
-    '''
+
     def land(self):
+        """
+        Lands at the current location
+        """
         self.get_global_info()
         lat = self.get_lat()
         lon = self.get_lon()
         self._the_connection.mav.command_long_send(self._the_connection.target_system, self._the_connection.target_component, mavutil.mavlink.MAV_CMD_NAV_LAND, 0, 0, 0, 0, 0, lat, lon, self._altitude)
         self.landed = True
+        
     def get_global_info(self):
+        """
+        Pulls the information of the plane of the getters
+        """
         msg = self._the_connection.recv_match(
             type='GLOBAL_POSITION_INT', blocking=True)
         msg = str(msg).split()
@@ -195,22 +203,56 @@ class Plane:
         self._global_info = [lat, lon, alt, velo_x, velo_y, velo_z]
     
     def get_lat(self):
+        """
+        Get the current lattitude
+        
+        :return the current lattiude as a float
+        """
         return self._global_info[0]
     
     def get_lon(self):
+        """
+        Get the current longitude
+        
+        :return the current longitude as a float
+        """
         return self._global_info[1]
     
     def get_alt(self):
+        """
+        Gets the current altitude
+        
+        :return the current altitde as an int
+        """
+        
         return self._global_info[2]
 
     def get_velo_x(self):
+        """
+        Get the current velocity onn the x plane
+        
+        :return the current x velocity
+        """
         return self._global_info[3]
     
     def get_velo_y(self):
+        """
+        Get the current velocity on the y plane
+        
+        :return the current y velocity
+        """
         return self._global_info[4]
     
     def get_velo_z(self):
+        """
+        Get the current velocity onn the z plane
+        
+        :return the current z velocity
+        """
         return self._global_info[5] 
         
     def set_geofence():
+        """
+        Sets up the geofence(s)
+        """
         asyncio.run(set_geofence_comp)
